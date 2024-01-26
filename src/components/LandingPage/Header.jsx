4aa759321ee8de92e3bd5220ba731a5924222725
O.G Ayanfe <ayanfeoluwakoyinsola@gmail.com>
2024-01-26 21:03:57 +0100

    Updated UI

--- src/components/LandingPage/Header.jsx
+++ src/components/LandingPage/Header.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 
+function Hamburger({ width, height }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width || "1em"}
+      height={height || "1lem"}
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M3 17h18M3 12h18M3 7h18"
+      />
+    </svg>
+  );
+}
 function Header() {
   return (
     <nav className="w-full pt-9 px-8 flex justify-between items-center absolute top-0">
-      <h1 className="text-2xl text-blue-600 font-bold flex gap-4 items-center justify-center">
+      <span className="md:hidden text-3xl absolute">
+        <Hamburger />
+      </span>
+      <a className="text-2xl text-blue-600 font-bold flex max-md:ml-12 gap-4 items-center justify-center">
         <img src="/vite.svg"></img> {/*This should be replaced with the actual logo*/}
-        <span>WishesUnbox</span>
-      </h1>
+        <span className="max-sm:hidden">WishesUnbox</span>
+      </a>
       <div className="flex gap-7 items-center justify-center text-md text-cyan-900 font-semibold font-[cursive]">
         <a href="#" className="hover:text-blue-500 max-md:hidden">
           Follow
@@ -14,7 +36,7 @@ function Header() {
         <a href="#" className="hover:text-blue-500 max-md:hidden">
           Dashboard
         </a>
-        <a className="w-40 text-blue-50 bg-blue-500 font-semibold text-md rounded-3xl py-2 text-center">
+        <a className="w-40 text-blue-50 bg-[#FF9A55] font-semibold text-md rounded-3xl py-2 text-center">
           Get started
         </a>
       </div>
--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -6,26 +6,26 @@ function LandingPage() {
   return (
     // <div className=" w-full bg-gradient-to-r from-pink-500 via-purple-500 to-red-500 background-animate">
     <div className="flex items-center flex-col justify-center w-full relative h-min">
-      <main className="w-full flex flex-col items-center max-w-7xl px-8 relative min-h-screen">
+      <main className="w-full flex flex-col items-center max-w-7xl px-8 pb-8 relative min-h-screen">
         <Header />
-        <div className="grid items-center justify-center flex-col pt-28 w-full gap-10 lg:grid-cols-2 min-h-screen">
-          <div className="">
-            <p className="text-3xl max-lgw:my-12 text-gray-900 max-lg:text-center">
+        <div className="grid items-center justify-center flex-col max-lg:pt-32 w-full gap-16 lg:grid-cols-2 min-h-screen">
+          <div className="mx-auto max-lg:max-w-lg">
+            <p className="text-2xl lg:text-3xl text-gray-900 max-lg:text-center">
               Personalized gift <br /> card creation platform.
             </p>
-            <p className="text-sm text-gray-900 max-lg:text-center py-2">
+            <p className="text-sm text-gray-900 max-lg:text-center py-4">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo eos est corrupti
-              perspiciatis libero debitis eaque. Velit voluptatem cumque, corrupti animi
+              perspiciatis libero debitis eaque. Velit voluptatem cumque
             </p>
             <div className="w-full max-lg:flex items-center justify-center">
-              <button className="bg-purple-600 text-purple-50 text-xl px-4 py-2 rounded-lg shadow-2xl mt-8">
+              <button className="bg-[#FF9A55] text-red-50 text-xl px-4 py-2 rounded-lg shadow-2xl mt-8">
                 Try It Out
               </button>
             </div>
           </div>
           <img
             src="/0.webp"
-            className="w-[800spx] rounded-3xl shadow-2xl max-h-[calc(100-30px)] shadow-black bg-blue-200"
+            className="max-w-[700px] w-full rounded-3xl shadow-2xl max-h-[calc(100-30px)] shadow-black bg-blue-200"
           ></img>
         </div>
       </main>
