a7179a4cb274140f65c9ec0513c58a33915d57d9
Black Bobs <ayodejiayodele350@gmail.com>
2024-01-31 02:38:07 +0100

    added some changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,12 +4,12 @@ import { FaArrowRight } from "react-icons/fa6";
 const Button = ({ text }) => {
   return (
     <button
-      className="bg-[#ff9a55] w-full p-5 rounded-full text-white capitalize text-2xl font-bold my-10 outline-none hover:bg-[#FFA478] hover:scale-95 transition-all flex items-center justify-center shadow-sm"
+      className="bg-[#ff9a55] w-full p-5 rounded-full text-white capitalize text-2xl font-bold my-8 outline-none hover:bg-[#FFA478] hover:scale-95 transition-all flex items-center justify-center shadow-sm"
       type="submit"
     >
       {text}{" "}
-      <span className="mx-3">
-        <FaArrowRight />
+      <span className="mx-2">
+        <FaArrowRight size={15} />
       </span>
     </button>
   );
--- src/sections/signupPage.jsx
+++ src/sections/signupPage.jsx
@@ -12,23 +12,16 @@ const Signup = () => {
   return (
     <section className="w-full h-full flex flex-col items-center justify-center">
       <form
-        className="w-full h-fit p-20 m-5 bg-neutral-500-100 rounded-md flex-1 md:max-w-[500px] shadow-lg relative"
+        className="w-full h-fit p-14 m-2 bg-neutral-500-100 rounded-md flex-1 md:max-w-[500px] shadow-lg relative"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div className="flex flex-col text-center items-center justify-center mb-10">
+        <div className="flex flex-col text-center items-center justify-center mb-5">
           <h2 className="text-center font-bold text-5xl p-2 uppercase bg-gradient-to-br from-[#ff9a55] to-[#ffb09c] text-transparent bg-clip-text">
             WishesUnbox
           </h2>
           <span className=" text-gray-400">register and enjoy our service</span>
         </div>
-        <div className="mt-20">
-          <div className="bg-black flex justify-center items-center p-3 rounded-full cursor-pointer">
-            <img src="/google.svg" alt="google" className="w-12" />
-            <p className="text-white mx-5 text-xl capitalize">
-              Sign up with Google
-            </p>
-          </div>
-        </div>
+
         <div>
           {/* Mapped over the input fields */}
           {signupFields.map((input) => {
@@ -38,7 +31,7 @@ const Signup = () => {
           })}
         </div>
         <Button text="signup" />
-        <div>
+        <div className="flex items-center justify-center">
           <span className="p-3 cursor-pointer">
             Already have an account?{" "}
             <a href="/" className="hover:underline text-[#ff9a55]">
@@ -46,8 +39,18 @@ const Signup = () => {
             </a>
           </span>
         </div>
-
-        <div className="absolute bottom-4 flex flex-1 items-center justify-between w-4/5 m-auto capitalize">
+        <div className="flex w-full my-10 items-center justify-center">
+          <hr className="flex-1" />
+          <p className="uppercase p-3 text-2xl">or</p>
+          <hr className="flex-1" />
+        </div>
+        <div className="bg-black flex justify-center items-center p-3 rounded-full cursor-pointer my-10">
+          <img src="/google.svg" alt="google" className="w-12" />
+          <p className="text-white mx-5 text-xl capitalize">
+            Sign up with Google
+          </p>
+        </div>
+        <div className="absolute bottom-4 flex flex-1 items-center justify-between w-4/5 m-auto capitalize py-2 mt-10">
           <a href="/" className="text-xl hover:text-[#ffa478]">
             Terms and conditions
           </a>
