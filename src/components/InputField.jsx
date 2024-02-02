9365d9662cac9026cb1cee8fa2e18e9ac3b2ea09
Black Bobs <ayodejiayodele350@gmail.com>
2024-02-02 23:54:05 +0100

    Reduce the font size

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 const InputField = ({ name, type, placeholder, register }) => {
   return (
-    <div className="my-5">
-      <label className="w-full block font-bold p-2 capitalize" htmlFor={name}>
+    <div className="my-1">
+      <label
+        className="w-full block font-bold p-2 capitalize text-[10px]"
+        htmlFor={name}
+      >
         {name}
       </label>
       <input
-        className="w-full block rounded-full p-5 outline-none bg-zinc-100 focus:border-2 border-[#ff9a55]"
+        className="w-full block rounded-full py-2 px-5 outline-none bg-zinc-100 focus:border-2 border-[#ff9a55] text-[10px]"
         type={type}
         id={name}
         {...register(name, { required: true })}
--- src/sections/loginPage.jsx
+++ src/sections/loginPage.jsx
@@ -16,10 +16,12 @@ const loginPage = () => {
         onSubmit={handleSubmit(onSubmit)}
       >
         <div className="flex flex-col text-center items-center justify-center mb-5">
-          <h2 className="text-center font-bold text-5xl p-2 uppercase bg-gradient-to-br from-[#ff9a55] to-[#ffb09c] text-transparent bg-clip-text">
+          <h2 className="text-center font-bold text-2xl p-2 uppercase bg-gradient-to-br from-[#ff9a55] to-[#ffb09c] text-transparent bg-clip-text">
             WishesUnbox
           </h2>
-          <span className=" text-gray-400">register and enjoy our service</span>
+          <span className=" text-gray-400 text-[10px]">
+            register and enjoy our service
+          </span>
         </div>
 
         <div>
@@ -34,10 +36,10 @@ const loginPage = () => {
         <Button text="login" />
 
         <div className="flex flex-col items-center justify-between">
-          <p className="p-2 text-[#ff9a55] cursor-pointer hover:underline">
+          <p className="p-2 text-[10px] text-[#ff9a55] cursor-pointer hover:underline">
             forget your password ?
           </p>
-          <p className="p-2 cursor-pointer">
+          <p className="p-2 text-[10px] cursor-pointer">
             No account yet?{" "}
             <a href="/" className="hover:underline hover:text-[#ff9a55]">
               Sign up
@@ -45,24 +47,24 @@ const loginPage = () => {
           </p>
         </div>
 
-        <div className="flex w-full my-5 items-center justify-center">
+        <div className="flex w-full my-2 items-center justify-center">
           <hr className="flex-1" />
-          <p className="uppercase p-3 text-2xl">or</p>
+          <p className="uppercase p-3 text-[8px]">or</p>
           <hr className="flex-1" />
         </div>
 
-        <div className="bg-black flex justify-center items-center p-3 rounded-full cursor-pointer mt-5 mb-10">
-          <img src="/google.svg" alt="google" className="w-12" />
-          <p className="text-white mx-5 text-xl capitalize">
+        <div className="bg-black flex justify-center items-center p-2 rounded-full cursor-pointer my-3">
+          <img src="/google.svg" alt="google" className="w-5" />
+          <p className="text-white mx-5 text-[8px] capitalize">
             Login with Google
           </p>
         </div>
 
-        <div className="flex flex-1 items-center justify-between w-4/5 m-auto capitalize">
-          <a href="/" className="text-xl hover:text-[#ffa478]">
+        <div className="flex flex-1 items-center justify-between w-4/5 mx-auto capitalize mt-10">
+          <a href="/" className="text-[8px] hover:text-[#ffa478]">
             Terms and conditions
           </a>
-          <a href="/" className="text-xl hover:text-[#ffa478]">
+          <a href="/" className="text-[8px] hover:text-[#ffa478]">
             Privacy policy
           </a>
         </div>
