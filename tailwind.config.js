141576d2ea81f9d68287b34c24b5f99fc274118e
Gglovie <joantetra@gmail.com>
2024-02-10 10:19:16 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontSize: {
+      'lg-main': '34px',
+      'lg-sub' : '24px',
+      'lg-body': '16px'
+    },
+    colors:{
+      'main-orange':'#FF9A55',
+      'light-orange':'#FFA478',
+      'beige':'#FFB09C',
+      'light-pink':'#FFBFC0',
+      'white':'#ffffff',
+      'black':'#000000',
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+      serif: ['Notable', 'serif'],
+      'montez': ['Montez', 'monospace']
+    }
   },
   plugins: [],
 }
