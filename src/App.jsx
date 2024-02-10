8c3242f7c149eedb36d839355ab08c3bad058dbe
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-02-10 15:31:46 +0100

    New branch updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import SignupPage from "./sections/signupPage";
 import LoginPage from "./sections/loginPage";
-import CreateCard from "./components/CreateCard/CreateCard";
 
 function App() {
   return (
     <>
       {/* <SignupPage /> */}
       <LoginPage />
-      <CreateCard />
     </>
   );
 }
